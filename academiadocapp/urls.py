1acc476aa5b30d7fa1902a742dc1db1c224b6a15
gabriel <mellchii@bu.edu>
2023-02-01 06:00:16 +0100

    fixed messaging bug on user login and logout

--- academiadocapp/urls.py
+++ academiadocapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="home"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
+    path('logout/', views.logout_view, name='logout'),
 
 ]
